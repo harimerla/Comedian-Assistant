2df94b88d7955b71118470df1d554ad304b8cb1d
harimerla <35225821+harimerla@users.noreply.github.com>
2024-04-01 20:07:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from langchain.llms import OpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage, BaseOutputParser
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
 import os
 import streamlit as st
 
@@ -6,19 +9,52 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+chat = ChatOpenAI(temperature=0.6)
+
+if 'flowmessages' not in st.session_state:
+    st.session_state['flowmessages'] = [
+        SystemMessage(content='You are a comedian AI assistant')
+    ]
+
 def get_response_from_openai(question):
-    llm = OpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'), temperature=0.6, model='gpt-3.5-turbo-instruct')
-    response = llm.predict(question)
-    return response
+    try:
+        st.session_state['flowmessages'].append(HumanMessage(content=question))
+        answer = chat(st.session_state['flowmessages'])
+        st.session_state['flowmessages'].append(AIMessage(answer.content))
+        return answer.content
+    except:
+        return "Error encountered"
 
 st.set_page_config(page_title='Q&A demo')
-st.header('LangChain Application')
+st.header('Comedian ChatBot')
 
-input = st.text_input('Input: ', key='input')
-response = get_response_from_openai(input)
+def reset_conversation():
+  st.session_state.conversation = None
+  st.session_state.chat_history = None
+  st.session_state['flowmessages'].clear()
+st.button('Reset Chat', on_click=reset_conversation)
 
-submit = st.button("Ask a question")
+# submit = st.button("Ask a question")
 
-if submit:
-    st.subheader('The Response is')
-    st.write(response)
+try:
+
+    for message in st.session_state['flowmessages']:
+        if message.type=='system':
+            continue
+        role='assistant'
+        if message.type=='human':
+            role='user'
+        with st.chat_message(role):
+            st.markdown(message.content)
+
+    if input := st.chat_input('Input: ', key='input'):
+        with st.chat_message('user'):
+            st.markdown(input)
+        with st.chat_message('assistant'):
+            response = get_response_from_openai(input)
+            st.markdown(response)
+            # st.subheader('The Response is')
+            # st.write(response)
+
+except:
+    pass
